8ddaee8d04fd61e5f614bbb283f4a4791fc8e3f3
calveto <todd.calvert@gmail.com>
2016-01-18 18:15:26 -0800

    WIP

--- tunnel_relic.go
+++ tunnel_relic.go
@@ -6,17 +6,170 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
+	//"os"
 	"strings"
-	"sync"
 	"time"
 )
 
-var tunnelLock sync.Mutex // Controls access to ruleset
+type Job struct {
+	Event     map[string]interface{}
+	EventType string
+}
+
+func (j Job) String() string {
+	j.Event["eventType"] = j.EventType
+	eventJson, err := json.Marshal(j.Event)
+	if err != nil {
+		fmt.Println("tunnelRelic: json Marshaling error", err)
+	}
+	return string(eventJson[:])
+}
+
+// A buffered channel that we can send work request on.
+var JobQueue = make(chan Job, 100)
+
+// Worker represents the worker that executes the job.
+type Worker struct {
+	WorkerPool chan chan Job
+	JobChannel chan Job
+	quit       chan bool
+	Config     *Tunnel
+}
+
+func NewWorker(workerPool chan chan Job, config *Tunnel) Worker {
+	w := Worker{
+		WorkerPool: workerPool,
+		JobChannel: make(chan Job),
+		quit:       make(chan bool)}
+
+	w.Config = config
+
+	return w
+}
+
+// Start method starts the run loop for the worker.
+func (w Worker) Start() {
+	go func() {
+		jobs := []Job{}
+		for {
+			// register the current worker into the worker queue
+			w.WorkerPool <- w.JobChannel
+
+			select {
+			case job := <-w.JobChannel:
+				// we have received a work request.
+				jobs = append(jobs, job)
+
+				if len(jobs) > w.Config.SendBuffer {
+					fmt.Println("---------------------- Doing some Work!! -------------------------")
+
+					// pop one job at a time off the slice
+					var jobBatch []Job
+					for i := 0; i < w.Config.SendBuffer; i++ {
+						job := Job{}
+						job, jobs = jobs[len(jobs)-1], jobs[:len(jobs)-1]
+						jobBatch = append(jobBatch, job)
+					}
+
+					// Check this out to make sure there is not race condition
+					// Run code with race detector
+					go w.SendBatch(jobBatch)
+					jobBatch = []Job{}
+				} else {
+					fmt.Printf("\nBuffering: We only have %d Jobs in the slice ---->\n", len(jobs))
+				}
+			case <-w.quit:
+				return
+			}
+		}
+	}()
+}
+
+func (w Worker) SendBatch(jobs []Job) {
+	var events []string
+	for x := range jobs {
+		events = append(events, jobs[x].String())
+	}
+	requestStr := "[" + strings.Join(events, ",") + "]"
+
+	var eventJson = []byte(requestStr)
+	fmt.Printf("\nSending Events to NR --------------------------->")
+	fmt.Printf("\nEvent Json: \n\n%s\n\n", eventJson)
+
+	req, err := http.NewRequest("POST", w.Config.InsightsURL, bytes.NewBuffer(eventJson))
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	req.Header.Set("X-Insert-Key", w.Config.InsightsAPI)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	// Need to check status codes and re-queue when appropriate.
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	if w.Config.Silent != true {
+		fmt.Println("tunnelRelic: Sending queued request to New Relic. Response: ", string(body))
+	}
+}
+
+// Stop the worker
+func (w Worker) Stop() {
+	go func() {
+		w.quit <- true
+	}()
+}
+
+type Dispatcher struct {
+	WorkerPool chan chan Job // A pool of workers channels that are registered with the dispatcher
+	Config     *Tunnel       // Configuration
+}
+
+func (relic *Tunnel) NewDispatcher(numWorkers int) *Dispatcher {
+	pool := make(chan chan Job, numWorkers)
+	return &Dispatcher{WorkerPool: pool, Config: relic}
+}
+
+// Start MaxWorkers number of Workers
+func (d *Dispatcher) Run() {
+	for i := 0; i < d.Config.MaxWorkers; i++ {
+		worker := NewWorker(d.WorkerPool, d.Config)
+		worker.Start()
+	}
+	go d.dispatch()
+}
+
+func (d *Dispatcher) dispatch() {
+	for {
+		select {
+		case job := <-JobQueue:
+			// a job request has been received
+			go func(job Job) {
+				// try to obtain a worker job channel that is available
+				// this will block until a worker is idle
+				jobChannel := <-d.WorkerPool
+
+				// dispatch the job to the worker job channel
+				jobChannel <- job
+			}(job)
+		}
+	}
+}
 
 type Tunnel struct {
 	SendInterval    int
 	SendBuffer      int
+	MaxWorkers      int
 	InsightsAPI     string
 	InsightsAccount string
 	InsightsEvent   string
@@ -25,67 +178,59 @@ type Tunnel struct {
 	SendQueue       []string
 }
 
-func NewTunnel(Account string, APIKey string, EventName string, send int, sendBuff int) *Tunnel {
+func NewTunnel(account string, apiKey string, eventName string, send int, sendBuff int, maxWorkers int) *Tunnel {
 
-	url := strings.Join([]string{"https://insights-collector.newrelic.com/v1/accounts/", Account, "/events"}, "")
+	url := strings.Join([]string{"https://nsights-collector.newrelic.com/v1/accounts/", account, "/events"}, "")
 	relic := &Tunnel{
-
 		SendInterval:    send,
 		SendBuffer:      sendBuff,
-		InsightsAPI:     APIKey,
-		InsightsAccount: Account,
+		InsightsAPI:     apiKey,
+		InsightsAccount: account,
 		InsightsURL:     url,
 		Silent:          false,
-		InsightsEvent:   EventName,
+		InsightsEvent:   eventName,
+		MaxWorkers:      maxWorkers,
 	}
 
-	go relic.MaintainQueue()
+	//	go relic.MaintainQueue()
+	dispatcher := relic.NewDispatcher(1)
+	dispatcher.Run()
+
 	return relic
 
 }
 
 func NewTransaction() map[string]interface{} {
 	newRelicTransaction := make(map[string]interface{})
-
-	if hostname, err := os.Hostname(); err == nil {
-		newRelicTransaction["host"] = hostname
-	} else {
-		newRelicTransaction["host"] = "default"
-	}
+	// Add common attributes for all events
+	/*
+		if hostname, err := os.Hostname(); err == nil {
+			newRelicTransaction["host"] = hostname
+		} else {
+			newRelicTransaction["host"] = "default"
+		}
+	*/
 
 	return newRelicTransaction
 }
 
+func (relic *Tunnel) RegisterEvent(event map[string]interface{}) {
+	// Create a Job
+	work := Job{Event: event, EventType: relic.InsightsEvent}
+
+	// Push the work on the queue
+	JobQueue <- work
+}
 func (relic *Tunnel) MaintainQueue() {
 
 	for true {
 		time.Sleep(time.Second * time.Duration(int64(relic.SendInterval)))
-		go relic.EmptyQueue()
+		//go relic.EmptyQueue()
 	}
 
 }
 
-func (relic *Tunnel) RegisterEvent(event map[string]interface{}) {
-	event["eventType"] = relic.InsightsEvent
-	eventJson, err := json.Marshal(event)
-	if err != nil && relic.Silent != true {
-		fmt.Println("tunnelRelic: Error receiving event", err)
-	}
-
-	objectString := string(eventJson[:])
-	tunnelLock.Lock()
-	defer tunnelLock.Unlock()
-	relic.SendQueue = append(relic.SendQueue, objectString)
-	if relic.Silent != true {
-		fmt.Println("tunnelRelic: Added event to send-queue. Currently ", len(relic.SendQueue), " events in the queue")
-	}
-	//fmt.Println(objectString)
-	if len(relic.SendQueue) > relic.SendBuffer && relic.Silent != true {
-		fmt.Println("tunnelRelic: Event queue buffer reached!")
-		go relic.EmptyQueue()
-	}
-}
-
+/*
 func (relic *Tunnel) EmptyQueue() {
 
 	if len(relic.SendQueue) < 1 {
@@ -125,3 +270,4 @@ func (relic *Tunnel) EmptyQueue() {
 	}
 
 }
+*/
